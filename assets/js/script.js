836c73adbda1ce90c82e72d21aeed71b12c3f529
codebystu <stubet17@outlook.com>
2024-07-09 10:09:14 +0100

    Add mapped out JS functions in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// pause for page load
+
+// How to play slider
+
+// New game button and initialise game
+
+// stake placing inc comparison with bank
+
+// deal button and event listener
+
+// card array and random card generator
+
+// initial deal write to DOM
+
+// player turn, buttons, event listener, hand calculation, add card to DOM, messaging
+
+// dealer turn, hand calculation, add card to DOM, hand comparison, messaging
+
+// update bank
+
+// loop back to stake placing
