9edba50e059228a0c4df71e271c9bed9c561b1a5
codebystu <stubet17@outlook.com>
2024-08-09 11:42:13 +0100

    Add playersecondcard function and timeouts for card functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
-// pause for page load
-
-
 // declare global variable hand arrays
 var playerHand = [];
 var dealerHand = [];
 var nextCard = [];
 var playerBlackjack = false;
 
-// How to play show and hide
+/** open and close game rules */ 
+
 function openRules() {
     document.getElementById("rules").style.display = "block";
   }
@@ -21,7 +19,7 @@ function openRules() {
   let close = document.getElementById("close-rules"); 
     close.addEventListener('click', closeRules);
 
-// New game button and initialise game
+/** New game button and initialise game */ 
 function newGame() {
   let displayBank = document.getElementById("cashbox");
   displayBank.textContent = 100;
@@ -35,23 +33,23 @@ function newGame() {
 let startGame = document.getElementById("new-game");
 startGame.addEventListener('click', newGame);
 
-// clears cards from table, resets hand arrays and displays buttons for placing stakes
+/** clears cards from table, resets hand arrays and displays buttons for placing stakes*/ 
 function newHand() {
   playerHand.length = 0;
   dealerHand.length = 0;
   nextCard.length = 0;
-
+  
 // clears cards from table. code adapted from https://javascript.plainenglish.io/how-to-remove-html-elements-by-class-name-b0288988dd55  
   const clear = document.querySelectorAll('.card')
 for (const el of clear) {
   el.parentNode.removeChild(el);
 }
   document.getElementById("message-box").textContent = "Place your bet";
-  document.getElementById("chip-buttons").style.display = "block";
-  
+  document.getElementById("chip-buttons").style.display = "block"; 
   placeBet();
-};
+}
 
+/** Handles player choice of button */
 function placeBet() {
   let dealbutton = document.getElementById("deal");
   let addchip = document.getElementById("addchip");
@@ -62,7 +60,7 @@ function placeBet() {
   removechip.addEventListener("click", reduceStake);
 }
 
-
+/** increments stake and reduces bank */
 function incrementStake() {
 
   let oldStake = parseInt(document.getElementById("stakebox").innerText);
@@ -74,8 +72,9 @@ function incrementStake() {
   } else {
     alert("Bankroll too low!");
   }
-};
+}
 
+/** reduces stake and increments bank */
 function reduceStake() {
 
   let oldStake = parseInt(document.getElementById("stakebox").innerText);
@@ -86,20 +85,21 @@ function reduceStake() {
   } else {
     alert("minimum stake is 1");
   }
-};
+}
 
+/** checks stake has been made and calls functions for initial deal */
 function newDeal() {
   let oldStake = parseInt(document.getElementById("stakebox").innerText);
   if (oldStake >= 1) {
   document.getElementById("message-box").textContent = "";
   document.getElementById("chip-buttons").style.display = "none";
   playerCard();
-  playerCard();
   } else {
     alert("minimum stake is 1");
   }
-};
+}
 
+/** Adds player card and handles result */
 function playerCard() {
   newCard();
   let playerContainer = document.getElementById('player-cards');
@@ -114,13 +114,8 @@ function playerCard() {
     document.getElementById("player-score").innerText = sum;
     }
   if (dealerHand.length < 1) {
-    dealerCard();
-  } else if ( sum === 21 && playerHand.length === 2 ) {
-    playerBlackjack = true;
-    document.getElementById("message-box").textContent = "Blackjack!";
-    dealerTurn();
+    setTimeout(dealerCard, 500); 
   } else if ( sum > 21 && playerHand.includes(11)) {
-    console.log("has ace")
     changeAce();
   } else if ( sum > 21) {
     playerBust();
@@ -129,24 +124,49 @@ function playerCard() {
   }
 }
 
+function playerSecondcard() {
+  newCard();
+  let playerContainer = document.getElementById('player-cards');
+    let playerCard = document.createElement('div');
+    playerCard.classList.add("card");
+    playerContainer.appendChild(playerCard).innerText = nextCard.face;
+    playerHand.push(nextCard.value);
+    let sum = playerHand.reduce((accumulator, current) => accumulator + current);
+    if ( sum === 21 && playerHand.length === 2 ) {
+      playerBlackjack = true;
+      document.getElementById("message-box").textContent = "Blackjack!";
+      setTimeout(dealerTurn, 1000);
+    } else if (playerHand.includes(11) && sum < 21) {
+      document.getElementById("player-score").innerText = `${sum - 10} / ${sum}`; 
+    } else {
+    document.getElementById("player-score").innerText = sum;
+    setTimeout(playerTurn, 500);
+    }
+    }
+
+/** Handles Ace value of 1/11 */
 function changeAce(){
   let index = playerHand.indexOf(11);
-
-  console.log(playerHand);
   playerHand.splice(index, 1);
-  console.log(playerHand);
   playerHand.push(1);
   let sum = playerHand.reduce((accumulator, current) => accumulator + current);
   document.getElementById("player-score").innerText = sum;
-  console.log(playerHand);
 }
 
+/** displays message when player bust */
 function playerBust() {
+  document.getElementById("player-buttons").style.display = "none";
   document.getElementById("message-box").textContent = "Bust! You Lose";
   youLose();
 }
 
+/** resets stake value after losing hand and calls newHand function */
+function youLose() {
+  document.getElementById("stakebox").innerText = 0;
+  newHand();
+}
 
+/** Adds card to dealer hand */
 function dealerCard() {
   newCard();
 
@@ -157,8 +177,14 @@ function dealerCard() {
     dealerHand.push(nextCard.value);
     let sum = dealerHand.reduce((accumulator, current) => accumulator + current);
     document.getElementById("dealer-score").innerText = sum;
-   
+    if (playerHand.length = 2) {
+      setTimeout(playerSecondcard, 500);
+    } else {
+      setTimeout(dealerTurn, 500);
+    }
 }
+
+/** handles player choice on extra card or stand */
 function playerTurn() {
   document.getElementById("player-buttons").style.display = "block";
 let extracard = document.getElementById("hit");
@@ -167,7 +193,6 @@ let endturn = document.getElementById("stand");
 extracard.addEventListener("click", playerCard);
 endturn.addEventListener("click", dealerTurn);
 }
-
 
 
 function dealerTurn() {
